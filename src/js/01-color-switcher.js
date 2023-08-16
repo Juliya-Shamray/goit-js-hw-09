5367d0d71451d67418978f1eb069be0fbd39a3b0
Juliya Shamray <fizmatka908990@gmail.com>
2023-08-16 09:00:51 +0300

    added task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,27 @@
+const start = document.querySelector('button[data-start]');
+const stop = document.querySelector('button[data-stop]');
 
+start.addEventListener('click', onStartClick);
+stop.addEventListener('click', onStopClick);
+
+let id;
+
+function onStartClick() {
+  id = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+    start.disabled = true;
+    stop.disabled = false;
+  }, 1000);
+}
+
+function onStopClick() {
+  clearInterval(id);
+  stop.disabled = true;
+  start.disabled = false;
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
